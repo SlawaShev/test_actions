9d5fd49e6a5a8d4b432dfe1bbe4aa1eb7f38b1c7
Шевелев Вячеслав Станиславович <Vyacheslav.Shevelev@mvideo.ru>
2021-01-05 17:29:07 +0300

    init commint

---
+++ dict_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+dict1 = {'aa': 'bb', 'cc': 'dd'}
+dict2 = {'ee': 'ff'}
+
+dict1.update(dict2)
+dict3 = dict1
+
+print(dict3)
+print(dict3["aa"])
+print(dict3["cc"])
+print(dict3["ee"])
